ce66bbc2b76583450aecd060c079a8034ac3ea22
artem <temaartem240904@gmail.com>
2024-04-12 23:58:20 +0300

    product list

--- myshop/shop/models.py
+++ myshop/shop/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.urls import reverse
 
 class Category(models.Model):
     name = models.CharField(max_length=200)
@@ -11,6 +12,10 @@ class Category(models.Model):
         ]
         verbose_name = 'categoty'
         verbose_name_plural = 'categories'
+
+    def get_absolute_url(self):
+        return reverse('shop:product_list_by_category',
+                       args=[self.slug])
 
     def __str__(self) -> str:
         return self.name
@@ -37,6 +42,10 @@ class Product(models.Model):
             models.Index(fields=['name']),
             models.Index(fields=['-created']),
         ]
+
+    def get_absolute_url(self):
+        return reverse('shop:product_detail',
+                       args=[self.id, self.slug])
 
     def __str__(self) -> str:
         return self.name
