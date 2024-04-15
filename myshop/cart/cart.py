7cbe7ae5ec781e5413334c81a7ed1e5e4de212f8
artem <temaartem240904@gmail.com>
2024-04-16 01:53:49 +0300

    cart view

--- myshop/cart/cart.py
+++ myshop/cart/cart.py
@@ -30,7 +30,7 @@ class Cart:
     def save(self):
         self.session.modified = True
 
-    def remote(self, product):
+    def remove(self, product):
         """
         delete from bascket
         """
@@ -61,10 +61,10 @@ class Cart:
         return sum(item['quantity'] for item in self.cart.values())
     
     def get_total_price(self):
-        return sum(Decimal(item['price'] * item['quantity'] 
-                           for item in self.cart.values()))
+        return sum(Decimal(item['price']) * item['quantity'] 
+                           for item in self.cart.values())
     
     def clear(self):
         del self.session[settings.CART_SESSION_ID]
         self.save()
-        
+
--- myshop/myshop/urls.py
+++ myshop/myshop/urls.py
@@ -5,7 +5,8 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('shop.urls', namespace='shop'))
+    path('cart/', include('cart.urls', namespace = 'cart')),
+    path('', include('shop.urls', namespace='shop')),
 ]
 
 if settings.DEBUG:
